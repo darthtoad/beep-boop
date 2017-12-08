c24c39727bfbd36a0d56a4dbdb5f8509487e07b8
Sam Gespass <30681859+darthtoad@users.noreply.github.com>
2017-12-08 10:34:02 -0800

    add extra silly rules

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,21 @@ var re0 = /[0]/;
 var re1 = /[1]/;
 
 var beepBoop = function(number) {
-  var array = []
+  var array = [];
   for (var i = 0; i <= number; i++) {
 //    debugger;
-    if (i.toString().match(re1)) {
+    if (i === 42) {
+      array.push("The answer to life, the universe, and everything")
+    } else if (i.toString().match(re1)) {
       array.push("Boop!");
     } else if (i.toString().match(re0)) {
-      array.push("Beep!")
+      array.push("Beep!");
     } else if (i % 3 === 0) {
-      array.push("I'm sorry, " + document.getElementById("name").value + ". I'm afraid I can't do that.")
+      array.push("I'm sorry, " + document.getElementById("name").value + ". I'm afraid I can't do that.");
+    } else if (i % 4 === 0) {
+      array.push("ALL YOUR BASE ARE BELONG TO US!");
+    } else if (i % 5 === 0) {
+      array.push("It follows the law of fives! Hail Eris! All hail Discordia!")
     } else {
       array.push(i);
     }
